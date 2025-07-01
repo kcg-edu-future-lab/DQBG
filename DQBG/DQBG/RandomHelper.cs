d3ac7241fec51b994bf92da4eee661d0b18a89c0
Keiho Sakapon <git@saka-pon.net>
2025-07-01 22:09:18 +0900

    Add helper classes

---
+++ DQBG/DQBG/RandomHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace DQBG
+{
+	public static class RandomHelper
+	{
+		static readonly Random random = new();
+
+		public static double NextDouble(double max) => random.NextDouble() * max;
+		public static double NextDouble(double min, double max) => min + random.NextDouble() * (max - min);
+		public static int NextInt(double value, double minRate, double maxRate) => (int)(value * NextDouble(minRate, maxRate));
+
+		public static IEnumerable<T> Shuffle<T>(this T[] a)
+		{
+			var b = (T[])a.Clone();
+			for (var i = a.Length - 1; i >= 0; --i)
+			{
+				var j = random.Next(i + 1);
+				yield return b[j];
+				b[j] = b[i];
+			}
+		}
+	}
+}
